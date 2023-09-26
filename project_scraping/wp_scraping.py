53cb3c65ac4c54fdd8cbae2bb2b5224f6a6e5123
adrianli375 <adrian.li.9375@gmail.com>
2023-09-27 00:26:18 +0800

    scraping competitors website hourly

--- project_scraping/wp_scraping.py
+++ project_scraping/wp_scraping.py
@@ -1,19 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
 from csvlogging import Log
+from datetime import datetime
+import time
 
-Logger = Log("wp.csv")
+def logging():
+    print(datetime.now(), "start logging")
+    Logger = Log("wp.csv")
 
-for i in range(1,101):
-    result = requests.get(fr"https://wp2023.cs.hku.hk/fyp23{i:03d}/")
-    if result.status_code != 200:
-        continue
-    soup = BeautifulSoup(result.text,"html.parser")
-    if soup.find("main") is not None:
-        headings = soup.find("main").getText().strip().replace("\n"," ")
-        Logger.log(headings[:min(len(headings),150)],i)
-    else:
-        print("Error",i)
-        Logger.log("Error",i)
- 
-print("done")
+    for i in range(1,101):
+        result = requests.get(fr"https://wp2023.cs.hku.hk/fyp23{i:03d}/")
+        if result.status_code != 200:
+            continue
+        soup = BeautifulSoup(result.text,"html.parser")
+        if soup.find("main") is not None:
+            headings = soup.find("main").getText().strip().replace("\n"," ")
+            Logger.log(headings[:min(len(headings),150)],i)
+        else:
+            print("Error",i)
+            Logger.log("Error",i)
+    
+    print("done")
+
+
+if __name__ == '__main__':
+    while True:
+        logging()
+        time.sleep(60*60)
