1af0122955f77fee1563afdc448d1f4c7cc2d7b9
adrianli375 <adrian.li.9375@gmail.com>
2023-11-04 14:36:09 +0800

    updated code for Crypto-Metadata-ML

---
+++ code/Crypto-Metadata-ML/loadModel.py
@@ -0,0 +1,99 @@
+from AlgorithmImports import *
+
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, StackingClassifier, VotingClassifier
+from sklearn.linear_model import LogisticRegressionCV
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
+from sklearn.svm import SVC
+
+def load_model(model_name: str):
+    if model_name not in {
+        'rf', 'lr', 'svm', 'gb', 'knn', 'stacking', 'votinghard', 'votingsoft'
+    }:
+        raise ValueError(f'Unsupported model name {model_name}!')
+
+    ### set hyperparameters here
+    # for rf
+    max_depth = 7 # [2, 5, 7]
+    # for svm
+    C = 1e-2 # [1e-4, 1e-2, 1, 10]
+    # for gb
+    learning_rate = 1e-4 # [1e-4, 1e-3, 1e-2]
+    n_estimators = 150 # [100, 150, 250]
+    # for knn
+    n_neighbours = 3 # [3, 5, 7]
+    
+    if model_name == 'rf':
+        # set hyperparameters
+        model =  make_pipeline(
+            StandardScaler(), 
+            RandomForestClassifier(random_state=4801, max_depth=max_depth)
+        )
+    elif model_name == 'lr':
+        model =  make_pipeline(
+            StandardScaler(), 
+            LogisticRegressionCV(random_state=4801, Cs=[1e-4, 1e-2, 1, 10], n_jobs=-1)
+        )
+    elif model_name == 'svm':
+        model = make_pipeline(
+            StandardScaler(), 
+            SVC(random_state=4801, probability=True, C=C)
+        )
+    elif model_name == 'gb':
+        model = make_pipeline(
+            StandardScaler(), 
+            GradientBoostingClassifier(random_state=4801, learning_rate=learning_rate, n_estimators=n_estimators)
+        )
+    elif model_name == 'knn':
+        model = make_pipeline(
+            StandardScaler(),
+            KNeighborsClassifier(n_neighbors=n_neighbours)
+        )
+    elif model_name == 'stacking':
+        # set individual hyperparameters first
+        model = make_pipeline(
+            StandardScaler(),
+            StackingClassifier(
+                estimators=[
+                    ('rf', RandomForestClassifier(random_state=4801, max_depth=max_depth)),
+                    ('lr', LogisticRegressionCV(random_state=4801, Cs=[1e-4, 1e-2, 1, 10], n_jobs=-1)),
+                    ('svm', SVC(random_state=4801, probability=True, C=C)),
+                    ('gb', GradientBoostingClassifier(random_state=4801, learning_rate=learning_rate, n_estimators=n_estimators)),
+                    ('knn', KNeighborsClassifier(n_neighbors=n_neighbours))
+                ],
+                n_jobs=-1
+            )
+        )
+    elif model_name == 'votinghard':
+         model = make_pipeline(
+            StandardScaler(),
+            VotingClassifier(
+                estimators=[
+                    ('rf', RandomForestClassifier(random_state=4801, max_depth=max_depth)),
+                    ('lr', LogisticRegressionCV(random_state=4801, Cs=[1e-4, 1e-2, 1, 10], n_jobs=-1)),
+                    ('svm', SVC(random_state=4801, probability=True, C=C)),
+                    ('gb', GradientBoostingClassifier(random_state=4801, learning_rate=learning_rate, n_estimators=n_estimators)),
+                    ('knn', KNeighborsClassifier(n_neighbors=n_neighbours))
+                ],
+                voting='hard',
+                n_jobs=-1
+            )
+        )
+    elif model_name == 'votingsoft':
+         model = make_pipeline(
+            StandardScaler(),
+            VotingClassifier(
+                estimators=[
+                    ('rf', RandomForestClassifier(random_state=4801, max_depth=max_depth)),
+                    ('lr', LogisticRegressionCV(random_state=4801, Cs=[1e-4, 1e-2, 1, 10], n_jobs=-1)),
+                    ('svm', SVC(random_state=4801, probability=True, C=C)),
+                    ('gb', GradientBoostingClassifier(random_state=4801, learning_rate=learning_rate, n_estimators=n_estimators)),
+                    ('knn', KNeighborsClassifier(n_neighbors=n_neighbours))
+                ],
+                voting='soft',
+                n_jobs=-1
+            )
+        )
+    return model
+
---
+++ code/Crypto-Metadata-ML/main.py
@@ -0,0 +1,202 @@
+from AlgorithmImports import *
+from QuantConnect.DataSource import *
+
+import pandas as pd
+import numpy as np
+from datetime import timedelta
+
+from loadModel import load_model
+
+class BitcoinMachineLearningAlgorithm(QCAlgorithm):
+    
+    def Initialize(self) -> None:
+        self.SetStartDate(2019, 9, 1)   # Set Start Date
+        self.SetEndDate(2022, 12, 31)    # Set End Date
+        self.SetCash(1000000)
+        self.SetBrokerageModel(BrokerageName.Binance, AccountType.Margin)
+        self.resolution = Resolution.Daily
+
+        ### Extra params to include
+        self.tradePeriod = 3
+        self.traded = False
+        self.placedTrade = self.Time
+        self.tradeConfidenceLevel = 0.6
+        self.tradeHistory = 252 * 5
+        self.npercent = 0.025
+        self.stopLossPercent = 0.1
+        self.sellatnpercent = True
+        self.sellatndays = True
+        self.extendWhenSignalled = True
+        self.updateExePriceWhenExtend = True # self.extendWhenSignalled must be true
+        self.sellWhenSignalChange = True
+
+        ### ML params
+        ### get the model name: choose one from {'rf', 'lr', 'svm', 'gb', 'knn', 'stacking', 'votinghard', 'votingsoft'}
+        self.modelName = 'votinghard'
+        self.predictionMechanism = 'soft' # one from {'soft', 'hard'}, 'soft' is preferred
+        
+        ### this crypto data is only available from the Bitfinex market
+        self.ticker = self.AddCrypto("BTCUSD", self.resolution, Market.Bitfinex).Symbol
+        ### Requesting data
+        self.metadataSymbol = self.AddData(BitcoinMetadata, self.ticker).Symbol 
+        
+        ### other fixed params
+        self.trainRatio = 0.8
+        self.model = None
+        self.lastDemandSupply = None
+        self.exePrice = 0
+
+        self.trainModel()
+
+    def trainModel(self) -> None:
+        ### preprocessing
+        ### Historical data
+        metadata = self.History(BitcoinMetadata, self.metadataSymbol, self.tradeHistory, self.resolution)
+        history = self.History(self.ticker, self.tradeHistory, self.resolution)#[['close', 'volume']]
+        self.Debug(history.shape)
+        self.Debug(f"We got {len(history)} items from our history request for {self.ticker} Blockchain Bitcoin Metadata")
+        # self.Debug(f"The time is {self.Time}")
+        history['closeAfterFewDays'] = history['close'].shift(-self.tradePeriod)
+        history['returns'] = history['closeAfterFewDays'] / history['close'] - 1
+        history.dropna(inplace=True)
+        history['signal'] = history['returns'].apply(lambda x: 1 if x > self.npercent else (-1 if x < -self.npercent else 0))
+        
+        metadata['signal'] = np.nan
+        metadataSignalIdx = list(metadata.columns).index('signal')
+        historySignalIdx = list(history.columns).index('signal')
+        for i in range(len(history)):
+            metadata.iat[i, metadataSignalIdx] = history.iat[i, historySignalIdx]
+        metadata.dropna(inplace=True)
+        
+        ### modelling
+        trainSize = int(self.tradeHistory * self.trainRatio)
+        X_train = metadata.drop(columns=['signal'])[:trainSize].values
+        y_train = metadata['signal'][:trainSize]
+
+        X_valid = metadata.drop(columns=['signal'])[trainSize:].values
+        y_valid = metadata['signal'][trainSize:]
+
+        self.model = load_model(self.modelName)
+        self.model.fit(X_train, y_train)
+
+        self.Log(f'The validation score of the model is {self.model.score(X_valid, y_valid)}')
+    
+    def OnData(self, slice: Slice) -> None:
+        ### Retrieving data
+        data = slice.Get(BitcoinMetadata)
+        if not data.ContainsKey(self.metadataSymbol):
+            return
+        latestMetadata = data[self.metadataSymbol]
+        X_pred = np.array([
+            latestMetadata.AverageBlockSize, latestMetadata.BlockchainSize,
+            latestMetadata.CostPercentofTransactionVolume, latestMetadata.CostPerTransaction, 
+            latestMetadata.Difficulty, 
+            latestMetadata.EstimatedTransactionVolume, latestMetadata.EstimatedTransactionVolumeUSD, 
+            latestMetadata.HashRate, latestMetadata.MarketCapitalization, 
+            latestMetadata.MedianTransactionConfirmationTime, latestMetadata.MinersRevenue, 
+            latestMetadata.MyWalletNumberofTransactionPerDay, latestMetadata.MyWalletNumberofUsers, 
+            latestMetadata.MyWalletTransactionVolume, latestMetadata.NumberofTransactionperBlock, 
+            latestMetadata.NumberofTransactions, latestMetadata.NumberofTransactionsExcludingPopularAddresses, 
+            latestMetadata.NumberofUniqueBitcoinAddressesUsed, latestMetadata.TotalBitcoins, 
+            latestMetadata.TotalNumberofTransactions, latestMetadata.TotalOutputVolume, 
+            latestMetadata.TotalTransactionFees, latestMetadata.TotalTransactionFeesUSD
+        ])
+        
+        if self.metadataSymbol in data and data[self.metadataSymbol] != None:
+            # get market data
+            currentPrice = slice.Bars[self.ticker].Close
+            currentDemandSupply = data[self.metadataSymbol].NumberofTransactions / data[self.metadataSymbol].HashRate
+            avgPrice = self.Portfolio[self.ticker].AveragePrice
+            positions = self.Portfolio[self.ticker].Quantity
+            cashAvailable = self.Portfolio.Cash
+
+            # get signals
+            predictedSignal = self.model.predict(np.reshape(X_pred, (1, -1))).astype(int)[0]
+            positiveSignal = predictedSignal == 1
+            negativeSignal = predictedSignal == -1
+            if self.predictionMechanism == 'hard':
+                modelConfidence = predictedSignal
+            elif self.predictionMechanism == 'soft': # 'soft'
+                try:
+                    modelConfidenceArr = self.model.predict_proba(np.reshape(X_pred, (1, -1)))
+                    if positiveSignal:
+                        modelConfidence = modelConfidenceArr[0][-1]
+                    elif negativeSignal:
+                        modelConfidence = modelConfidenceArr[0][0]
+                    else: # neutral signal
+                        modelConfidence = 0
+                except AttributeError: # if predict_proba is not available
+                    modelConfidence = predictedSignal
+
+            # comparing the average transaction-to-hash-rate ratio changes, we will buy bitcoin or hold cash
+            #if self.lastDemandSupply != None and currentDemandSupply > self.lastDemandSupply:
+            if abs(positions * currentPrice) > 100:
+                # update exePrice
+                if self.exePrice == 0:
+                    self.exePrice = avgPrice
+                sell = False
+
+                # if sell at n days is true, we sell it
+                if self.sellatndays:
+                    if (self.placedTrade + timedelta(days=self.tradePeriod)) >= self.Time:
+                        sell = True
+                
+                # check if the signal is extended
+                if self.extendWhenSignalled:
+                    if (modelConfidence >= self.tradeConfidenceLevel and positions > 0) or (modelConfidence <= -self.tradeConfidenceLevel and positions < 0):
+                        self.placedTrade = self.Time
+                        if self.updateExePriceWhenExtend:
+                            self.exePrice = currentPrice
+                        self.Log('time extended')
+
+                # check if signal is changed to sell
+                if self.sellWhenSignalChange:
+                    if (modelConfidence >= self.tradeConfidenceLevel and positions > 0) or (modelConfidence <= -self.tradeConfidenceLevel and positions < 0):
+                        sell = True
+                        self.Log('signal changed to sell')
+
+                # take profit
+                if not sell and self.sellatnpercent:
+                    self.DefaultOrderProperties.TimeInForce = TimeInForce.Day
+                if positions > 0:
+                    self.LimitOrder(self.ticker, -positions, self.exePrice * (1 + self.npercent), tag='take profit from long position')
+                else:
+                    self.LimitOrder(self.ticker, -positions, self.exePrice * (1 - self.npercent), tag='take profit from short position')
+                
+                # stop loss
+                if not sell and self.stopLossPercent > 0:
+                    self.DefaultOrderProperties.TimeInForce = TimeInForce.Day
+                    if positions > 0:
+                        if currentPrice < self.exePrice * (1 - self.stopLossPercent):
+                            self.DefaultOrderProperties.TimeInForce = TimeInForce.GoodTilCanceled
+                            self.MarketOrder(self.ticker, -positions)
+                            self.Log('stop loss triggered from long position')
+                        else:
+                            self.StopLimitOrder(self.ticker, -positions, self.exePrice * (1 - self.stopLossPercent), self.exePrice * (1 - self.stopLossPercent))
+                    else:
+                        if currentPrice > self.exePrice * (1 + self.stopLossPercent):
+                            self.DefaultOrderProperties.TimeInForce = TimeInForce.GoodTilCanceled
+                            self.MarketOrder(self.ticker, -positions)
+                            self.Log('stop loss triggered from short position')
+                        else:
+                            self.StopLimitOrder(self.ticker, -positions, self.exePrice * (1 + self.stopLossPercent), self.exePrice * (1 + self.stopLossPercent))
+                
+                # if sell triggered
+                if sell:
+                    self.DefaultOrderProperties.TimeInForce = TimeInForce.GoodTilCanceled
+                    self.MarketOrder(self.ticker, -positions)
+            else:
+                self.exePrice = 0
+                self.DefaultOrderProperties.TimeInForce = TimeInForce.GoodTilCanceled
+                if modelConfidence >= self.tradeConfidenceLevel:
+                    quantity = int(cashAvailable / currentPrice)
+                    self.MarketOrder(self.ticker, quantity)
+                    self.placedTrade = self.Time
+                elif modelConfidence <= -self.tradeConfidenceLevel:
+                    quantity = int(cashAvailable / currentPrice)
+                    quantity = int(cashAvailable / currentPrice)
+                    self.MarketOrder(self.ticker, -quantity)
+                    self.placedTrade = self.Time
+                            
+            # finally, update the latest demand supply of the blockchain
+            self.lastDemandSupply = currentDemandSupply
